bdab7b70164c6f6b526760d52b123eb46ac1ccda
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-04-24 15:06:13 +0900

    ターン数が一定以上経過したら強制終了するように修正

--- game.php
+++ game.php
@@ -29,9 +29,14 @@
       
       //現在の位置を表す配列
       $now_positions = array();
+      //トータルの移動を表した配列
+      $totals = array();
+
       foreach($this->players as $player) {
         $now_positions[] = $player->returnNowPosition();
+        $totals[] = 0;
       }
+
 
       //どちらかがゴールするまで繰り返し
       while ($now_positions[0] != $squares && $now_positions[1] != $squares) {
@@ -46,9 +51,11 @@
           switch($name) {
             case "Taro":
                $now_position = $now_positions[0];
+               $total = $totals[0];
             break;
             case "Jiro":
               $now_position = $now_positions[1];
+              $total = $totals[1];
             break;
           }
 
@@ -62,12 +69,15 @@
           //サイコロ振る
           $number = $this->throwDice();
           echo $number . "の目がでました。</br>" . $number . "マス進みます。</br>";
-          //現在のマス目にサイコロでてた数分を足す。
+          //現在のマス目にサイコロでてた数分を足す
           $now_position += $number;
+          //トータル移動距離にサイコロででた分をたす
+          $total += $number;
 
           if ($now_position == $squares) {
             //ちょうどゴールに到達した場合、ゲーム終了
             echo $name. "はゴールしました！.</br>" . $name . "の勝ちです！";
+            echo $name . "は合計で" . $total . "マス移動しました";
             exit();
           } else if ($now_position > $squares) {
             //足した数がゴールより大きくなった場合、そのぶんだけ戻る
@@ -89,20 +99,38 @@
             } else {
               if ($effect > 0) {
                 echo "マス目には" . $effect . "進むと書いてあります。" . $effect . "マス進みます！</br>";
+                if ($effect - ($squares - $now_position) > $squares) {
+                  //ゴールから折り返してスタート地点まで戻った場合
+                  $total += ($squares - $now_position) + $squares;
+                } else {
+                  $total += $effect;
+                }
               } else if ($effect < 0) {
                 echo "マス目には" . abs($effect) . "戻ると書いてあります。" . abs($effect) . "マス戻ります。。</br>";
+                if ($now_position - $effect < 0) {
+                  //スタートまで戻った場合
+                  $total += $now_position;
+                } else {
+                  $total += abs($effect);
+                }
               } else {
                 echo "マス目には何も書いてありませんでした。</br>";
               break;
               }
               $now_position += $effect;
-              
-              //足した数がゴールより大きくなった場合、そのぶんだけ戻る
-              if ($now_position > $squares) {
+
+              if ($now_position == $squares) {
+                //マス目の指示でゴールに到達した場合もゲーム終了
+                echo $name. "はゴールしました！" . $name . "の勝ちです！";
+                echo $name . "は合計で" . $total . "マス移動しました";
+                exit();
+              // }
+              } else if ($now_position > $squares) {
+                //足した数がゴールより大きくなった場合、そのぶんだけ戻る
                 echo "ちょうどゴール出来なかった場合、その分だけ戻ります。</br>";
                 $now_position = $squares - ($now_position - $squares);
-              }
-
+              } 
+              
               //マス目が0より小さくなった場合はスタート地点から
               if ($now_position < 0) {
                 echo "これ以上戻れないのでスタート地点まで戻ります。</br>";
@@ -126,19 +154,52 @@
             echo "現在の位置は" . $now_position . "マス目です。</br></br>";
            }
 
-          //現在の位置を格納
+          //現在の位置とトータルの移動距離を格納
           switch($name) {
             case "Taro":
               $now_positions[0] = $now_position;
+              $totals[0] = $total;
             break;
             case "Jiro":
               $now_positions[1] = $now_position;
+              $totals[1] = $total;
             break;
           }
 
         }
         //ターンカウント
         $turn ++;
+
+        //ターンが40以上かかる場合、トータルの移動距離を表示して強制終了
+        if ($turn > 40) {
+          echo "決着がつきません。すごろくのマス目に問題があるかもしれません。</br>";
+          foreach($this->players as $player) {
+            $name = $player->returnName();
+            switch($name) {
+              case "Taro":
+                $total = $totals[0];
+              break;
+              case "Jiro":
+                $total = $totals[1];
+              break;
+            }
+            echo $name . "は合計で" . $total . "マスだけ移動しました。</br>";
+          }
+          exit();
+        }
+        foreach($this->players as $player) {
+          $name = $player->returnName();
+          switch($name) {
+            case "Taro":
+              $total = $totals[0];
+            break;
+            case "Jiro":
+              $total = $totals[1];
+            break;
+          }
+          echo $name . "は合計で" . $total . "マスだけ移動しました。</br>";
+        }
+        echo "</br>";
       }       
     }
 
